902cd34f62558e7ef3842bcda813a67e5fcc8526
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-03-19 15:54:48 +0100

    MvcRequestHandler put all work inside exception handler.

--- Source/MvcRequestHandler.cs
+++ Source/MvcRequestHandler.cs
@@ -7,18 +7,14 @@ namespace Concoct
 {
     class MvcRequestHandler : IHttpListenerRequestHandler
     {
-        public void Process(HttpListenerContext context)
-        {
+        public void Process(HttpListenerContext context) {
             var httpContext = new HttpListenerContextAdapter(context);
-            var data = RouteTable.Routes.GetRouteData(httpContext);
-            var request = new RequestContext(httpContext, data);
-            var handler = data.RouteHandler.GetHttpHandler(request);
-            try
-            {
+            try {
+                var data = RouteTable.Routes.GetRouteData(httpContext);
+                var request = new RequestContext(httpContext, data);
+                var handler = data.RouteHandler.GetHttpHandler(request);
                 handler.ProcessRequest(httpContext.AsHttpContext());
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 httpContext.Response.StatusCode = 500;
                 httpContext.Response.Write(e.ToString());
             }
