0f0ce1db10a63096d2604c536a1a5fa4e108e681
unknown <drunkcod@.(none)>
2010-04-06 22:22:16 +0200

    Moving stuff into Stuff.

--- Source/TypeExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Concoct
-{
-    static class TypeExtensions
-    {
-        public static bool IsTypeOf<T>(this Type type){ return typeof(T).IsAssignableFrom(type); }
-    }
-}
