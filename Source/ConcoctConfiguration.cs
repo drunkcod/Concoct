a263b66642b3e392de8ea4ce5b17d67b97cdca25
drunkcod <i.am@drunkencoder.com>
2010-11-18 16:23:16 +0100

    Externalized configuration.

---
+++ Source/ConcoctConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace Concoct
+{
+    class ConcoctConfiguration
+    {
+        public ConcoctConfiguration() {
+            Port = 80;
+            Host = IPAddress.Any;
+        }
+
+        public string ApplicationAssemblyPath;
+        public int Port;
+        public IPAddress Host;
+        public string VirtualDirectoryOrPrefix;
+
+        public IPEndPoint GetEndPoint() { return new IPEndPoint(Host, Port); }
+    }
+}
--- Source/Program.cs
+++ Source/Program.cs
@@ -1,33 +1,47 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Web;
 using Xlnt.Stuff;
 
 namespace Concoct
 {
+    public class ConfigurationErrorException : Exception
+    { }
+
     public class Program
     {
         static int Main(string[] args) {
-            if(args.Length != 2) {
+            try {
+
+                var configuration = ParseConfiguration(args);
+            
+                var site = Assembly.LoadFrom(configuration.ApplicationAssemblyPath);
+                var types = site.GetTypes();
+                var httpApplicationType = types.Where(x => x.IsTypeOf<HttpApplication>()).First();
+                var host = MvcHost.Create(configuration.GetEndPoint(), configuration.VirtualDirectoryOrPrefix, Environment.CurrentDirectory, httpApplicationType);
+
+                host.Start();
+                Console.WriteLine("Listening for connections.");
+                Console.WriteLine("<press any key to quit>");
+                Console.ReadKey();
+                host.Stop();
+
+                return 0;
+            } catch(ConfigurationErrorException e) {
                 Console.Error.WriteLine("Usage is {0} <assembly> <virtual-directory>", Path.GetFileName(typeof(Program).Assembly.Location));
                 return -1;
             }
-            
-            var site = Assembly.LoadFrom(args[0]);
-            var types = site.GetTypes();
-            var httpApplicationType = types.Where(x => x.IsTypeOf<HttpApplication>()).First();
-            var host = MvcHost.Create(new IPEndPoint(IPAddress.Any, 80), args[1], Environment.CurrentDirectory, httpApplicationType);
+        }
 
-            host.Start();
-            Console.WriteLine("Listening for connections.");
-            Console.WriteLine("<press any key to quit>");
-            Console.ReadKey();
-            host.Stop();
-
-            return 0;
+        static ConcoctConfiguration ParseConfiguration(string[] args) {
+            if(args.Length != 2)
+                throw new ConfigurationErrorException();
+            return new ConcoctConfiguration {
+                ApplicationAssemblyPath = args[0],
+                VirtualDirectoryOrPrefix = args[1]
+            };
         }
     }
 }
