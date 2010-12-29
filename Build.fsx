02811f66db17dc3e36059575a4f4bb1e6f8a40dd
drunkcod <i.am@drunkencoder.com>
2010-12-29 09:23:13 +0100

    build.bat convinence & README added.

--- Build.fsx
+++ Build.fsx
@@ -14,29 +14,28 @@ let clean =
     | _ -> false 
   Seq.forall safeDirectoryDelete
 
-let SolutionPath = "Concoct.sln"
-
-let build args =
+let build solution args =
   let fxPath = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory()  
   let msBuild4 = Path.Combine(fxPath, @"..\v4.0.30319\MSBuild.exe")
   let build =
     Process.Start(
       ProcessStartInfo(
         FileName = msBuild4,
-        Arguments = SolutionPath + " /nologo " + args,
+        Arguments = solution + " /nologo " + args,
         UseShellExecute = false))
   build.WaitForExit()
   build.ExitCode = 0
 
 let package() =
     let version = AssemblyName.GetAssemblyName("Bin\Concoct.exe").Version.ToString()
-
     use zip = new ZipFile()
-
     zip.AddDirectory("Bin", "Bin") |> ignore
     zip.Save(@"Bin\Concoct-" + version + ".zip")
     true
 
-clean ["Build";"Bin"]
-&& build "/m /p:Configuration=Release"
-&& package()
+let r = 
+    clean ["Build";"Bin"] 
+    && build "Concoct.sln" "/m /p:Configuration=Release"
+    && package()
+if not r then
+    Console.Error.WriteLine("Build failed.")
