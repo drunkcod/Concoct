7c1e32970518f587812db4e4e936ed6a7306313a
drunkcod <i.am@drunkencoder.com>
2011-07-22 13:42:07 +0200

    create a few less temporary strings.

--- Source/Web/MultiPartStream.cs
+++ Source/Web/MultiPartStream.cs
@@ -114,17 +114,26 @@ namespace Concoct.Web
                 AtBoundary
             }
 
+            public struct LineSegment
+            {
+                public int Start, Count;
+            }
+
+            public delegate void PartReadyHandler(byte[] data, List<LineSegment> lines, int headerEndPosition);
+
             readonly MemoryStream partData = new MemoryStream();
-            readonly MultiPartBoundaryBuffer boundry;
+            readonly MultiPartBoundaryBuffer boundary;
             readonly byte[] boundaryBytes;
-            readonly Action<byte[], int> partReady;
+            readonly PartReadyHandler partReady;
             State state;
-            int headerEndPosition;
+            readonly List<LineSegment> lines = new List<LineSegment>();
+            int lineStart;
+            int bodyStartPosition;
             bool isFinished;
 
-            public BodyReader(byte[] boundaryBytes, Action<byte[], int> partReady) {
+            public BodyReader(byte[] boundaryBytes, PartReadyHandler partReady) {
                 this.boundaryBytes = boundaryBytes;
-                this.boundry = new MultiPartBoundaryBuffer(boundaryBytes.Length, partData.WriteByte);
+                this.boundary = new MultiPartBoundaryBuffer(boundaryBytes.Length, partData.WriteByte);
                 this.state = State.Buffering;
                 this.partReady = partReady;
             }
@@ -132,35 +141,50 @@ namespace Concoct.Web
             public bool IsFinished { get { return isFinished; } }
 
             public IMultiPartStreamState ProcessByte(byte value) {
-                boundry.WriteByte(value);
+                boundary.WriteByte(value);
                 switch(state) {
                     case State.Buffering:               
-                        if(headerEndPosition == 0 && boundry.EndsWith(HeaderSeparator))
-                        headerEndPosition = (int)partData.Position + boundry.Size;
-                        if(boundry.Matches(boundaryBytes)) {
-                            boundry.Discard();
+                        if(bodyStartPosition == 0) {
+                            var index = (int)partData.Position + boundary.Size;
+                            if(boundary.EndsWith(HeaderSeparator))
+                                bodyStartPosition = index;
+                            else if(boundary.EndsWith(LineSeparator)) {
+                                lines.Add(new LineSegment { Start = lineStart, Count = index - lineStart - LineSeparator.Length });
+                                lineStart = index;
+                            }
+                        }
+                        if(boundary.Matches(boundaryBytes)) {
+                            boundary.Discard();
                             state = State.AtBoundary;
                         }
                         break;
 
                     case State.AtBoundary:
-                        if(boundry.Size < 2)
+                        if(boundary.Size < 2)
                             break;
 
-                        var partSeparator = boundry.Matches(LineSeparator);
-                        isFinished = boundry.Matches(Dash, Dash);
+                        var partSeparator = boundary.Matches(LineSeparator);
+                        isFinished = boundary.Matches(Dash, Dash);
                         Debug.Assert(partSeparator || isFinished);
 
-                        boundry.Discard();
-                        if(partData.Position != 0) {
-                            partReady(partData.ToArray(), headerEndPosition);
-                            partData.Position = 0;
-                            headerEndPosition = 0;
-                        }
+                        boundary.Discard();
+                        if(HasPartData)
+                            PartReady();
+
                         state = State.Buffering;
                         break;
                 }
                 return this;
+            }
+
+            bool HasPartData { get { return partData.Position != 0; } }
+
+            void PartReady() {
+                partReady(partData.ToArray(), lines, bodyStartPosition);
+                partData.Position = 0;
+                bodyStartPosition = 0;
+                lineStart = 0;
+                lines.Clear();
             }
         }
 
@@ -171,7 +195,7 @@ namespace Concoct.Web
             BoundaryPrefix.CopyTo(boundaryBytes, 0);
             Encoding.GetBytes(boundary, 0, boundary.Length, boundaryBytes, BoundaryPrefix.Length);
 
-            var body = new BodyReader(boundaryBytes, (bytes, headerEndPosition) => OnPartReady(ReadPart(bytes, headerEndPosition)));
+            var body = new BodyReader(boundaryBytes, (bytes, lines, headerEndPosition) => OnPartReady(ReadPart(bytes, lines, headerEndPosition)));
             state = new HeaderReader(boundaryBytes, body);
         }
 
@@ -204,7 +228,7 @@ namespace Concoct.Web
 
         public static KeyValuePair<string, string> ParseHeader(string input) {
             var boundary = input.IndexOf(':');
-            return new KeyValuePair<string,string>(input.Substring(0, boundary), input.Substring(boundary + 1).Trim());
+            return new KeyValuePair<string,string>(input.Substring(0, boundary), input.Substring(boundary + 1).TrimStart());
         }
 
         byte ReadByte(Stream stream) {
@@ -213,15 +237,16 @@ namespace Concoct.Web
             return (byte)b;                  
         }
 
-        MimePart ReadPart(byte[] bytes, int headerEndPosition) {
+        MimePart ReadPart(byte[] bytes, List<BodyReader.LineSegment> lines, int headerEndPosition) {
             var part = new MimePart(new ArraySegment<byte>(bytes, headerEndPosition, bytes.Length - headerEndPosition));
             
-            using(var headerReader = new StreamReader(new MemoryStream(bytes, 0, headerEndPosition), Encoding.ASCII)) {
-                for(string line; (line = headerReader.ReadLine()) != "";) {
-                    var header = ParseHeader(line);
-                    part.Add(header.Key, header.Value);
-                }
+            for(var i = 0; i != lines.Count; ++i) {
+                var segment = lines[i];
+                var line = Encoding.GetString(bytes, segment.Start, segment.Count);
+                var header = ParseHeader(line);
+                part.Add(header.Key, header.Value);
             }
+
             return part;
         }
 
